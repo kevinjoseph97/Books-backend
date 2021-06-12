8a2c5d1a0b91aa12c56a2d5cbc9b213440117f23
kevinjoseph97 <kevinjoseph97@gmail.com>
2021-06-12 14:49:43 -0400

    adds controller action

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -6,8 +6,23 @@ class Api::V1::BooksController < ApplicationController
     end
     
     def create 
+        @book - Book.new(books_params)
+        if @book.save
+            render json: @book, status: :accepted
+        else
+            render json: {errors: book.errors.full_messages}, status:
+            :unprocessible_entity
 
+        end
     end 
+
+
+
+    private 
+
+    def books_params
+        params.require(:book).permit(:title, :author, :book_img, :genre_id)
+    end
 
 
 end
